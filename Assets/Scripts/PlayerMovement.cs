8163949daa09d9876f3a4b9489a9a65971c98a19
tr <jettzeong@gmail.com>
2021-12-14 21:21:47 +0800

    part 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,37 @@ public class PlayerMovement : MonoBehaviour
 {
 	public int speed;
 	private Vector3 change;
+	private Animator animator;
 	private Rigidbody2D myRigidBody;
 	private void Start()
 	{
 		myRigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
-	private void Update()
+	private void FixedUpdate()
 	{
+		change = Vector3.zero;
 		change.x = Input.GetAxisRaw("Horizontal");
 		change.y = Input.GetAxisRaw("Vertical");
-
-		if (change != Vector3.zero)
-		{
-			MoveCharacter();
-		}
+		UpdateAnimationAndMove();
 	}
 	public void MoveCharacter()
 	{
+		change.Normalize();
 		myRigidBody.MovePosition (transform.position + change * speed * Time.deltaTime);
-		
 	}
+	public void UpdateAnimationAndMove()
+	{
+		if (change != Vector3.zero)
+		{
+			MoveCharacter();
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
+		}
+		else
+		{
+			animator.SetBool("moving", false);
+		}
+	}	
 }
